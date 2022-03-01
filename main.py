9ed9be3f121e3c4cbbcb884b1af56b1ce7c0925e
ds-vasilev <ds_vasilev@mail.ru>
2022-03-01 17:31:37 +0300

    Изменены входные и выходные данные функций в соответствии с требованиями тестов

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Any
+from typing import Tuple
 
 
 def records_find(i_mas: int, index_i: int, massive: Any) -> None:
@@ -7,7 +8,7 @@ def records_find(i_mas: int, index_i: int, massive: Any) -> None:
     после чего рекурсивно вызывает себя для соседних ячеек.
     '''
     if i_mas >= 0 and i_mas < len(massive) and index_i >= 0 and index_i < len(massive[0]):
-        if massive[i_mas][index_i] == '1':
+        if massive[i_mas][index_i] == 1:
             massive[i_mas][index_i] = 0
             records_find(i_mas - 1, index_i, massive)
             records_find(i_mas + 1, index_i, massive)
@@ -15,21 +16,27 @@ def records_find(i_mas: int, index_i: int, massive: Any) -> None:
             records_find(i_mas, index_i + 1, massive)
 
 
-def calculate() -> None:
+def calculate(lst: tuple) -> None:
     '''
-    Получаем данные из файла как  двумерный массив, при нахождении единицы
-    вызывает рекурсивную функцию, по выходу из рекурсии добавляет в счетчик +1.
+    При нахождении единицы вызывает рекурсивную функцию,
+    по выходу из которой добавляет в счетчик +1.
     '''
-    with open('src.txt', 'r') as file:
-        lst = file.readlines()
-    lst = [[str(n) for n in x.split()] for x in lst]
+
     cr_count = 0
     for i_mas, val_mas in enumerate(lst):
         for index_i, val_i in enumerate(val_mas):
-            if val_i == '1':
+            if val_i == 1:
                 records_find(i_mas, index_i, lst)
                 cr_count += 1
-    return print(int(cr_count))
+    return int(cr_count)
 
 
-calculate()
+def read_from_file() -> Tuple:
+    '''Получаем данные из файла как  двумерный массив.'''
+    with open('src.txt', 'r') as file:
+        lst = file.readlines()
+    lst = [[int(n) for n in x.split()] for x in lst]
+    return lst
+
+
+calculate(read_from_file())
